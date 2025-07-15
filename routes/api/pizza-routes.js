9056e1c48d177b950225b841691aa983f202b257
Brent Feuerbacher <feuerbacherb@gmail.com>
2025-07-15 10:06:55 -0500

    created routing for api

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const { getAllPizza, getPizzaById, createPizza, updatePizza, deletePizza } = require("../../conrollers/pizza-controller");
+
+// Set up GET all and POST at /api/pizzas
+router.route("/").get(getAllPizza).post(createPizza);
+
+// set up GET one, PIT, and DELETE at /api/pizzas/:id
+router.route("/:id").get(getPizzaById).put(updatePizza).delete(deletePizza);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
+// import all of the API routes from /api/index.js (no need for index.js though since it is implied)
+const apiRoutes = require("./api");
 const htmlRoutes = "./html/html-routers";
 
-router.use("/", htmlRouters);
+// add prefix of `/api` to all of the api routes imported from the `api` directory
+router.use("/api", apiRoutes);
+
+router.use("/", htmlRoutes);
 
 router.use((req, res) => {
   res.status(404).send("<h1>😝 404 Error!");
